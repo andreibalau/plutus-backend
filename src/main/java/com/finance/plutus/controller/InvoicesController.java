13d9c6dc44610feb3ffb67b0e45249b0e380eead
Catalin M <catalin.matache@fortech.ro>
2020-09-08 11:56:44 +0300

    - expose invoice download endpoint

--- src/main/java/com/finance/plutus/controller/InvoicesController.java
+++ src/main/java/com/finance/plutus/controller/InvoicesController.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import com.finance.plutus.controller.payload.CreateInvoiceRequest;
 import com.finance.plutus.controller.payload.DownloadInvoiceRequest;
-import com.finance.plutus.controller.payload.DownloadInvoiceResponse;
 import com.finance.plutus.controller.payload.DownloadInvoicesRequest;
 import com.finance.plutus.controller.payload.EntityCreatedResponse;
 import com.finance.plutus.controller.payload.FindInvoiceResponse;
@@ -23,6 +22,11 @@ import com.finance.plutus.service.invoice.DownloadInvoiceService;
 import com.finance.plutus.service.invoice.FindInvoiceService;
 import com.finance.plutus.service.invoice.InvoiceCommandInvoker;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -84,20 +88,40 @@ public class InvoicesController {
       value = "/pdf/single",
       consumes = APPLICATION_VND_PLUTUS_FINANCE_JSON,
       produces = APPLICATION_VND_PLUTUS_FINANCE_JSON)
-  public DownloadInvoiceResponse downloadSingle(
+  public ResponseEntity<Resource> downloadSingle(
       @Valid @RequestBody DownloadInvoiceRequest request) {
     byte[] pdf = downloadInvoiceService.download(request.getInvoiceId());
-    return new DownloadInvoiceResponse(pdf);
+    HttpHeaders headers = new HttpHeaders();
+    headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=invoice.pdf");
+    headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+    headers.add("Pragma", "no-cache");
+    headers.add("Expires", "0");
+    ByteArrayResource resource = new ByteArrayResource(pdf);
+    return ResponseEntity.ok()
+        .headers(headers)
+        .contentLength(pdf.length)
+        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+        .body(resource);
   }
 
   @PostMapping(
       value = "/pdf/multiple",
       consumes = APPLICATION_VND_PLUTUS_FINANCE_JSON,
       produces = APPLICATION_VND_PLUTUS_FINANCE_JSON)
-  public DownloadInvoiceResponse downloadMultiple(
+  public ResponseEntity<Resource> downloadMultiple(
       @Valid @RequestBody DownloadInvoicesRequest request) {
     byte[] zip = downloadInvoiceService.downloadAll(request.getInvoicesIds());
-    return new DownloadInvoiceResponse(zip);
+    HttpHeaders headers = new HttpHeaders();
+    headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=archive.zip");
+    headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+    headers.add("Pragma", "no-cache");
+    headers.add("Expires", "0");
+    ByteArrayResource resource = new ByteArrayResource(zip);
+    return ResponseEntity.ok()
+        .headers(headers)
+        .contentLength(zip.length)
+        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+        .body(resource);
   }
 
   @ResponseStatus(NO_CONTENT)
