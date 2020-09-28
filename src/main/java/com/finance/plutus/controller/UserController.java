cd9d43697f1c7c72f8be5bfa178f5d13c78bbb09
Catalin M <catalin.matache@fortech.ro>
2020-09-28 10:28:35 +0300

    - split login and register controller

--- src/main/java/com/finance/plutus/controller/UserController.java
+++ src/main/java/com/finance/plutus/controller/UserController.java
@@ -1,21 +1,14 @@
 package com.finance.plutus.controller;
 
-import com.finance.plutus.controller.payload.LoginRequest;
-import com.finance.plutus.controller.payload.LoginResponse;
+import static com.finance.plutus.configuration.Api.APPLICATION_VND_PLUTUS_FINANCE_JSON;
+
 import com.finance.plutus.controller.payload.ProfileUserResponse;
-import com.finance.plutus.controller.payload.RegisterRequest;
-import com.finance.plutus.model.dto.LoggedUserDto;
 import com.finance.plutus.model.dto.UserDto;
 import com.finance.plutus.service.user.FindUserService;
-import com.finance.plutus.service.user.LoginService;
-import com.finance.plutus.service.user.RegisterUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-
-import static com.finance.plutus.configuration.Api.APPLICATION_VND_PLUTUS_FINANCE_JSON;
-import static org.springframework.http.HttpStatus.CREATED;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /** Plutus Created by catalin on 7/1/2020 */
 @RestController
@@ -23,26 +16,7 @@ import static org.springframework.http.HttpStatus.CREATED;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
-  private final LoginService loginService;
-  private final RegisterUserService registerUserService;
   private final FindUserService findUserService;
-
-  @ResponseStatus(CREATED)
-  @PostMapping(
-      value = "/new",
-      consumes = APPLICATION_VND_PLUTUS_FINANCE_JSON,
-      produces = APPLICATION_VND_PLUTUS_FINANCE_JSON)
-  public void register(@Valid @RequestBody RegisterRequest request) {
-    registerUserService.register(request.getUser());
-  }
-
-  @PostMapping(
-      consumes = APPLICATION_VND_PLUTUS_FINANCE_JSON,
-      produces = APPLICATION_VND_PLUTUS_FINANCE_JSON)
-  public LoginResponse login(@Valid @RequestBody LoginRequest request) {
-    LoggedUserDto user = loginService.login(request.getUsername(), request.getPassword());
-    return new LoginResponse(user);
-  }
 
   @GetMapping(
       consumes = APPLICATION_VND_PLUTUS_FINANCE_JSON,
--- src/main/java/com/finance/plutus/security/SecurityConfig.java
+++ src/main/java/com/finance/plutus/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.finance.plutus.security;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,9 +13,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 /** Plutus Created by catalin on 7/1/2020 */
 @EnableWebSecurity
@@ -31,13 +31,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     http.authorizeRequests()
         .antMatchers(
-            "/api/v1/users/new",
+            "/api/v1/login",
+            "/api/v1/register",
             "/v2/api-docs",
             "/swagger-ui.html/**",
             "/swagger-resources/**",
             "/webjars/**")
-        .permitAll()
-        .antMatchers(HttpMethod.POST, "/api/v1/users")
         .permitAll()
         .antMatchers(HttpMethod.GET, "/api/v1/users")
         .hasAuthority(ROLE_USER)
